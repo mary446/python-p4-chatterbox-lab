f483bbc89c2bf305da8412f01bc699f3a68e8dce
mary446 <135317707+mary446@users.noreply.github.com>
2023-09-22 13:38:23 +0300

    chatterbox complete

--- server/models.py
+++ server/models.py
@@ -1,28 +1,3 @@
-# from flask_sqlalchemy import SQLAlchemy
-# from sqlalchemy import MetaData
-# from sqlalchemy_serializer import SerializerMixin
-# from sqlalchemy.sql import func
-# from sqlalchemy import DateTime
-# metadata = MetaData(naming_convention={
-#     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-# })
-
-# db = SQLAlchemy(metadata=metadata)
-
-# class Message(db.Model, SerializerMixin):
-#     __tablename__ = 'messages'
-
-#     id = db.Column(db.Integer, primary_key=True)
-#     body = db.Column(db.String)
-#     username = db.Column(db.String)
-#     created_at = db.Column(db.DateTime, default=func.now())
-#     updated_at = db.Column(db.DateTime, onupdate = func.now())
-
-    
-
-#     def __repr__(self):
-#         return f"Message(id={self.id}, body='{self.body}', username='{self.username}')"
-     
 from sqlalchemy import func
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_serializer import SerializerMixin
